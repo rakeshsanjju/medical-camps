ca8d9c8514a6f7c78f4a97035623995320134e45
unknown <sanjurakeshdasari4@gmail.com>
2026-08-14 13:45:48 +0530

    Uploadings

--- back-end/server.js
+++ back-end/server.js
@@ -18,7 +18,9 @@ app.use(
     cors({
         origin: [
             "http://127.0.0.1:5501",
-            "http://localhost:5501"
+            "http://localhost:5501",
+            "http://localhost:3001",
+            "*"
         ],
         methods: [
             "GET",
